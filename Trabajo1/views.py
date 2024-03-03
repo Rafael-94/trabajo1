d1df9367bbbdec3b246311507498810952ec7776
Rafael Zarate <rrzarate4@misena.edu.co>
2024-03-02 19:52:47 -0500

    lo que llevo

---
+++ Trabajo1/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render 
+
+
+
+def inicio(request):
+  return render (request, 'index.html',{})
+
+def portafolio(request):
+  return render (request, 'portafolio.html',{})
+
+
